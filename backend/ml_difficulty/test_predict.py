a2e670db9022cf3fdfc9bd64b4443320e9284636
Fithi Fessehatsion <f2justice16@gmail.com>
2025-07-02 22:12:22 -0700

    Fixed classification & added topic modeling API

--- backend/ml_difficulty/test_predict.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/predict_difficulty'
-data = {'text': 'Quantum computing is a challenging field in computer science.'}
-
-response = requests.post(url, json=data)
-print(response.json())
