9fb30ce048d8de23073b9a80c5b46f9b5e3d6a09
nazanin-ts <hashemiannazi@yahoo.com>
2025-07-20 13:47:52 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import Layout from './components/Layout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import DifficultyPage from './pages/DifficultyPage';
+import Chatbot from './pages/Chatbot';
+import Flashcards from './pages/Flashcards';
+import Summarization from './pages/Summarization';
 
 function App() {
   return (
-    <Layout />
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/difficulty" element={<DifficultyPage />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/flashcards" element={<Flashcards />} />
+          <Route path="/summarization" element={<Summarization />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
