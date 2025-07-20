e82c7eb1664393df657c6eea7e954e20582b3fd5
justfifi17 <justfifi16@gmail.com>
2025-07-19 22:42:40 -0700

    Enabled CORS

--- backend/ML/ml_api.py
+++ backend/ML/ml_api.py
@@ -1,20 +1,19 @@
 # ✅ File: ml_api.py
 from flask import Flask
+from flask_cors import CORS
 from routes.classify import classify_bp
 from routes.topic_modeling import topic_bp
 from routes.analyze import analyze_bp
 import os
 
-
 app = Flask(__name__)
+CORS(app)  # Enables CORS for all routes
 
-# Register the classification route
+# Register blueprints
 app.register_blueprint(classify_bp)
 app.register_blueprint(topic_bp)
 app.register_blueprint(analyze_bp)
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render sets this env variable
     app.run(host="0.0.0.0", port=port)
-
