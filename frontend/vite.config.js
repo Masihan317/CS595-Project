a4604c4f16d3a433a25fc3baf239a1542f9d4ed8
nazanin-ts <hashemiannazi@yahoo.com>
2025-07-20 21:06:35 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +9,8 @@ export default defineConfig({
   resolve: {
     alias: {}
   },
-  base: "./"
-});
+  base: "./",
+  server: {
+    historyApiFallback: true
+  }
+})
